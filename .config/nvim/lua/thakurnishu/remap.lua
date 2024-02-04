b8c4931e8685453b0438875a16bc7d0e49e25ca2
thakurnishu <90508814+thakurnishu@users.noreply.github.com>
2024-02-04 16:46:36 +0530

    made some minor changes

--- .config/nvim/lua/thakurnishu/remap.lua
+++ .config/nvim/lua/thakurnishu/remap.lua
@@ -1,6 +1,15 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Open Three Facing Terminal
+vim.keymap.set("n", "<C-t>", function()
+  vim.cmd(":tabnew term://zsh")
+  vim.cmd("vsplit term://zsh")
+  vim.cmd("split term://zsh")
+  -- vim.cmd(':wincmd l', 'n', true)
+  -- vim.cmd("split term://zsh")
+end)
+
 -- Open Four Facing Terminal
 vim.keymap.set("n", "<C-t>", function()
   vim.cmd(":tabnew term://zsh")
