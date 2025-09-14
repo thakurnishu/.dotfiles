02bca25208c41880caf7b03ca35c99989668bb42
thakurnishu <90508814+thakurnishu@users.noreply.github.com>
2025-09-14 15:42:20 +0530

    update

--- .config/nvim/lua/config/set.lua
+++ .config/nvim/lua/config/set.lua
@@ -5,6 +5,17 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
+-- Use real tabs in Makefiles
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "make",
+  callback = function()
+    vim.opt_local.expandtab = false
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.softtabstop = 4
+  end,
+})
+
 
 -- disable vim backup but allow untree to get backup file
 vim.opt.swapfile = false
