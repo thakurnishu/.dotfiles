9a51d8c00f4d8b4b1e5d0d7949f2d075d16dd224
thakurnishu <90508814+thakurnishu@users.noreply.github.com>
2026-04-10 14:21:36 +0530

    update

--- .config/nvim/after/plugin/obsidian.lua
+++ .config/nvim/after/plugin/obsidian.lua
@@ -3,7 +3,7 @@ require("obsidian").setup({
   workspaces = {
     {
       name = "Notes",
-      path = "~/Desktop/src/obsidian/mahakal_vault",
+      path = "~/Desktop/src/github.com/personal/second_brain",
     },
   },
   notes_subdir = "MainNotes",
